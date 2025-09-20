213977f874ab376b2c51353b626269ef801e4823
Cazaks <calebezak@gmail.com>
2025-09-20 01:57:35 +0100

    Connecting to TaskController to have my first endpoint

--- ToDo_Back_End/toDoApplication/src/main/java/com/todoApplication/controllers/TaskController.java
+++ ToDo_Back_End/toDoApplication/src/main/java/com/todoApplication/controllers/TaskController.java
@@ -1,7 +1,9 @@
 package com.todoApplication.controllers;
 
 import com.todoApplication.datas.models.Task;
-import com.todoApplication.servises.TaskService;
+import com.todoApplication.dtos.requests.TaskRequestDTO;
+import com.todoApplication.dtos.responses.TaskResponseDTO;
+import com.todoApplication.services.TaskService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,18 +21,18 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<Task>> getAllTasks() {
+    @GetMapping("/getAllTask")
+    public ResponseEntity<List<TaskResponseDTO>> getAllTasks() {
         return ResponseEntity.ok(taskService.findAll());
     }
 
-    @PostMapping
-    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+    @PostMapping("/addTask")
+    public ResponseEntity<TaskResponseDTO> addTask(@RequestBody Task task) {
 
-        if(task.getTittle() == null || task.getTittle().isBlank()) {
+        if(task.getTitle() == null || task.getTitle().isBlank()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tittle is required");
         }
-        Task createdTask = taskService.addTask(task);
+        TaskResponseDTO createdTask = taskService.addTask(new TaskRequestDTO());
         return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
     }
 
--- ToDo_Back_End/toDoApplication/src/main/java/com/todoApplication/dtos/requests/TaskRequestDTO.java
+++ ToDo_Back_End/toDoApplication/src/main/java/com/todoApplication/dtos/requests/TaskRequestDTO.java
@@ -14,4 +14,5 @@ public class TaskRequestDTO {
     private String title;
     private String description;
     private LocalDate dueDate;
+    private String email;
 }
